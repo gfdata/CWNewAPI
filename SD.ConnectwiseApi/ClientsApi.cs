304002b7fad77010f58691ec120cca27dff5fcdc
Glenn Ferrie <glenn.ferrie@gmail.com>
2014-04-08 10:53:38 -0400

    more goodness for clients

---
+++ SD.ConnectwiseApi/ClientsApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace SD.ConnectwiseApi
+{
+    public sealed class ClientsApi
+        : ServiceBase
+    {
+        public IEnumerable<ServiceTicketInfo> FindServiceTickets(FindServiceTicketRequest request)
+        {
+            var filterExpression = string.Join(" AND ", request.Filters.Select(q => q.ToString()).ToArray());
+            var message = string.Format(MessageConstants.ServiceTickets_FindTickets, filterExpression);
+
+            var doc = new XmlDocument();
+            var resultXml = ProcessAction(message);
+            doc.LoadXml(resultXml);
+
+            return doc.DocumentElement.ChildNodes.Cast<XmlNode>()
+                    .First(q => "Tickets".Equals(q.Name))
+                    .ChildNodes.Cast<XmlNode>()
+                    .Select(q => ServiceTicketInfo.Create(q));
+
+        }
+    }
+}
---
+++ SD.ConnectwiseApi/Model/FindClientsRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.ConnectwiseApi;
+
+namespace SD.ConnectwiseApi
+{
+    public sealed class FindClientsRequest
+    {
+        public List<FilterCriteria<ClientProperties>> Filters { get; set; }
+
+        public InvoiceProperties OrderBy { get; set; }
+
+        public int Skip { get; set; }
+
+        public int Limit { get; set; }
+    }
+}
